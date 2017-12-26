73d20c40aec4021a655e97ed9f3af451fc575c62
Philip Riecks <philip@riecks.io>
2017-12-26 16:51:16 +0100

    intial commit

---
+++ src/main/java/de/rieckpil/SpringCloudMicroserviceExampleApplication.java
@@ -0,0 +1,12 @@
+package de.rieckpil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudMicroserviceExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudMicroserviceExampleApplication.class, args);
+	}
+}
