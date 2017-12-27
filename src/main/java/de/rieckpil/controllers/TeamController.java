674a645307076672b21baa2dc23ff4f2a770108b
Philip Riecks <philip@riecks.io>
2017-12-27 15:46:16 +0100

    add Spring Data Rest endpoints

--- src/main/java/de/rieckpil/controllers/TeamController.java
+++ src/main/java/de/rieckpil/controllers/TeamController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController
+//@RestController
 public class TeamController {
 
     private TeamRepository teamRepository;
--- src/main/java/de/rieckpil/repositories/PlayerRepository.java
+++ src/main/java/de/rieckpil/repositories/PlayerRepository.java
@@ -1,9 +1,9 @@
 package de.rieckpil.repositories;
 
-import de.rieckpil.domain.Team;
+import de.rieckpil.domain.Player;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
 
-@RestResource(path="teams", rel = "team")
-public interface TeamRepository extends CrudRepository<Team, Long> {
+@RestResource(path="players", rel="player")
+public interface PlayerRepository extends CrudRepository<Player, Long> {
 }
